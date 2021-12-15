fefad7edb55cc5a4a6beb39614c58a6fff2870e3
D.licious <73747069+vaminic@users.noreply.github.com>
2021-12-15 14:05:55 -0500

    Create day8.js

---
+++ day8.js
@@ -0,0 +1,33 @@
+//Solution:
+const countActiveAstronauts = (roster) => {
+  //array.length finds the size of the given array
+  var count = roster.length;
+  return count;
+}
+
+/*
+INSTRUCTIONS
+Your task is to create a function that takes in a roster 
+array, and will calculate the amount of astronauts in 
+the given roster list and return said amount.
+
+Sometimes things are just simple, code doesn't need 
+to be complicated!
+*/
+
+/*
+EXAMPLES
+  Input:
+    const exampleRoster = [
+      {
+        firstName:"Chris",
+        lastName: "Hadfield",
+        nickname:"Space Oddity",
+        prefix:"Astronaut",
+        job:"Shuttle DJ"
+      }
+    ]
+    
+  Output:
+    1
+*/
