4db04fc7e5592e48370beee697d268e7c683bb61
D.licious <73747069+vaminic@users.noreply.github.com>
2021-12-15 13:49:16 -0500

    moved instructions to bottom

--- day2.js
+++ day2.js
@@ -1,4 +1,18 @@
+//Solution:
+const generateAstronautTag = (astronaut) => {
+  
+  var prefix = astronaut.prefix;
+  var firstName = astronaut.firstName;
+  var nickname = astronaut.nickname;
+  var lastName = astronaut.lastName;
+  
+  var out = prefix + ': ' + firstName + ' "' + nickname + '" ' + lastName;
+  
+  return out;
+}
+
 /*
+INSTRUCTIONS
 Registering new astronauts was handled manually in
 previous missions but it's taking too much time.
 Knowing that the astronaut information is already
@@ -11,26 +25,14 @@ parameter and return a string using the properties of
 the astronaut.*/
 
 /*
-Example:
-  Input:
-    const exampleAstronaut = {
-          firstName: "Yuri",
-          lastName: "Gagarin",
-          nickname: "First!",
-          prefix: "Cosmonaut"
-        }
-  Output:
-    Cosmonaut: Yuri "First!" Gagarin
+EXAMPLES
+Input:
+  const exampleAstronaut = {
+        firstName: "Yuri",
+        lastName: "Gagarin",
+        nickname: "First!",
+        prefix: "Cosmonaut"
+      }
+Output:
+  Cosmonaut: Yuri "First!" Gagarin
 */
-
-const generateAstronautTag = (astronaut) => {
-  
-  var prefix = astronaut.prefix;
-  var firstName = astronaut.firstName;
-  var nickname = astronaut.nickname;
-  var lastName = astronaut.lastName;
-  
-  var out = prefix + ': ' + firstName + ' "' + nickname + '" ' + lastName;
-  
-  return out;
-}
