4de329e49c57b0a5b4089e19f7cb79fc368fbe08
D.licious <73747069+vaminic@users.noreply.github.com>
2021-12-15 13:43:14 -0500

    Create day3.js

---
+++ day3.js
@@ -0,0 +1,38 @@
+/*
+Making sure that a value is within a specific range is 
+a common task in programming. In this case, the 
+structure given is a gauge object with a current 
+value, a minimum and a maximum. If the current 
+value is between the minimum and maximum, then 
+everything is peachy, if not, we return a message 
+saying that there is trouble.
+
+Your task is to create a function that will take the 
+gauge object as a parameter, and will return true if 
+the current value is between the minimum and 
+maximum, and return false if the value is outside 
+those.
+*/
+
+/*
+Examples:
+  Input:
+    const exampleGauge = {
+      current:0.4,
+      min:0.1,
+      max:0.9
+    }
+  Output:
+    true
+*/
+
+//Solution:
+const checkGaugeStatus = (gauge) => {
+
+  if (gauge.current >= gauge.min && gauge.current <= gauge.max) {
+    return true;
+  } else {
+    return false;
+  }
+  
+}
