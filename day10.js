4122a9d3323e3a47524fea09f6ca01f8340d3b8e
D.licious <73747069+vaminic@users.noreply.github.com>
2021-12-15 14:34:27 -0500

    Update day10.js

--- day10.js
+++ day10.js
@@ -27,22 +27,22 @@ using loops.
 
 /*
 EXAMPLE
-  Input:
-    const exampleEntries = [
-      { 
-        temperature:0, 
-        weather:"sunny", 
-        windDirection: "NNE", 
-        windSpeed:24
-      },
-      { 
-        temperature:10, 
-        weather:"cloudy", 
-        windDirection: "NNE", 
-        windSpeed:9 
-      }
-    ] 
-  
-  Output:
-    17
+Input:
+  const exampleEntries = [
+    { 
+      temperature:0, 
+      weather:"sunny", 
+      windDirection: "NNE", 
+      windSpeed:24
+    },
+    { 
+      temperature:10, 
+      weather:"cloudy", 
+      windDirection: "NNE", 
+      windSpeed:9 
+    }
+  ] 
+
+Output:
+  17
 */
