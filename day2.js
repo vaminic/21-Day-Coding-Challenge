bc6f84f83e4ab0f9e3ba491ea733ce6bcbc6a691
D.licious <73747069+vaminic@users.noreply.github.com>
2021-12-15 13:37:57 -0500

    Create day2.js

---
+++ day2.js
@@ -0,0 +1,36 @@
+/*
+Registering new astronauts was handled manually in
+previous missions but it's taking too much time.
+Knowing that the astronaut information is already
+listed in a data set, you propose to optimize that job
+with programming.
+
+Your task is to automate that process by creating a
+function that will take in an astronaut object as a
+parameter and return a string using the properties of
+the astronaut.*/
+
+/*
+Example:
+  Input:
+    const exampleAstronaut = {
+          firstName: "Yuri",
+          lastName: "Gagarin",
+          nickname: "First!",
+          prefix: "Cosmonaut"
+        }
+  Output:
+    Cosmonaut: Yuri "First!" Gagarin
+*/
+
+const generateAstronautTag = (astronaut) => {
+  
+  var prefix = astronaut.prefix;
+  var firstName = astronaut.firstName;
+  var nickname = astronaut.nickname;
+  var lastName = astronaut.lastName;
+  
+  var out = prefix + ': ' + firstName + ' "' + nickname + '" ' + lastName;
+  
+  return out;
+}
