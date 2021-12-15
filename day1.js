dddd18aff8af49668fb1a2d2febdbfd6690ff708
D.licious <73747069+vaminic@users.noreply.github.com>
2021-12-15 13:39:58 -0500

    changed comment format

--- day1.js
+++ day1.js
@@ -1,24 +1,25 @@
-//Our latest testing indicates a malfunctioning 
-//component in our communications apparatus. For 
-//some reason, messages from mission control are not 
-//being correctly displayed on board the shuttle. While 
-//the message is being displayed on the shuttle’s 
-//terminal, the sender’s name is not displayed beside it.
+/*
+Our latest testing indicates a malfunctioning 
+component in our communications apparatus. For 
+some reason, messages from mission control are not 
+being correctly displayed on board the shuttle. While 
+the message is being displayed on the shuttle’s 
+terminal, the sender’s name is not displayed beside it.
 
-//Your first task is to write a function that will take two 
-//string parameters and return a string containing the 
-//sender name and the message in the form specified in 
-//the instructions.
+Your first task is to write a function that will take two 
+string parameters and return a string containing the 
+sender name and the message in the form specified in 
+the instructions.
+*/
 
-//Solution:
+/*
+Example:
+  Input:
+    parseMessage("Mission Control", "Hello there!")
+  Output:
+     Mission Control: Hello there!
+*/
 const parseMessage = (origin, message) => {
-  // Code here!
-
-  // Want to see what's going on inside your function? 
-  // You can use console.log() to debug and print variables to the console below.
- 
-  // Remember to return a value!
   var out = origin + ": " + message;
-  
   return out;
 }
