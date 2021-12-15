d632d45129b2f0bb5127f8234fa76f603e4406c4
D.licious <73747069+vaminic@users.noreply.github.com>
2021-12-15 14:34:01 -0500

    Update day08.js

--- day08.js
+++ day08.js
@@ -17,17 +17,17 @@ to be complicated!
 
 /*
 EXAMPLES
-  Input:
-    const exampleRoster = [
-      {
-        firstName:"Chris",
-        lastName: "Hadfield",
-        nickname:"Space Oddity",
-        prefix:"Astronaut",
-        job:"Shuttle DJ"
-      }
-    ]
+Input:
+  const exampleRoster = [
+    {
+      firstName:"Chris",
+      lastName: "Hadfield",
+      nickname:"Space Oddity",
+      prefix:"Astronaut",
+      job:"Shuttle DJ"
+    }
+  ]
     
-  Output:
-    1
+Output:
+  1
 */
